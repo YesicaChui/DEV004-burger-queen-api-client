c9cb55308f688b897b624d80c8ee6559c3c5b60f
YesicaChui <yesicachuic@gmail.com>
2023-05-02 12:31:04 -0500

    agregando ruteo de las pantallas mesera, cocina, admin y login(/)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+// https://ultimatecourses.com/blog/programmatically-navigate-react-router
 // Componente login
 export const Login = () => {
+  const navigate = useNavigate();
   const [usuario, setUsuario] = useState('');
   const [contrasena, setContrasena] = useState('');
-  const logearse = () => {
+  const inputUsuario =useRef(null)
+  const logearse = (usuario,contrasena) => {
+    console.log(`valor capturado desde el ref ${inputUsuario.current.value}`)
     console.log('Correo Usuario:', usuario);
     console.log('Contraseña:', contrasena);
     axios.post("http://localhost:8080/login", {
@@ -17,7 +22,12 @@ export const Login = () => {
     })
       .then(response => {
         console.log(response.data)
-        alert("Login Exitoso")
+        console.log(response.data.user.role)
+        //alert("Login Exitoso")
+        if(response.data.user.role === 'admin')  return navigate('/admin');
+        if(response.data.user.role === 'chef')  return navigate('/cocina');
+        if(response.data.user.role === 'waiter')  return navigate('/mesera');
+
       })
       // si el codigo es de 400 para arriba
       .catch(error => {
@@ -31,7 +41,8 @@ export const Login = () => {
         <div className="mb-3">
           <label htmlFor="inputUsuario" className="form-label">Correo de Usuario</label>
           <input
-            id="inputUsuario"
+            id="inputUsuario" 
+            ref={inputUsuario}
             type="text"
             className="form-control" placeholder="Correo de Usuario"
             value={usuario}
@@ -49,7 +60,7 @@ export const Login = () => {
             onChange={(e) => setContrasena(e.target.value)}
           />
         </div>
-        <button type="button" className="btn btn-primary" onClick={logearse}>Ingresar</button>
+        <button type="button" className="btn btn-primary" onClick={()=>logearse(usuario,contrasena)}>Ingresar</button>
       </form>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Login } from './components/Login.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Login/>,
+    errorElement:<h1>error ruta</h1>
+  },
+  {
+    path: "/mesera",
+    element:<h1>Mozo/a</h1>
+  },
+  {
+    path: "/cocina",
+    element:<h1>Cocina</h1>
+  },
+  {
+    path: "/admin",
+    element:<h1>Admnistracion</h1>
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+/*   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>, */
+  <RouterProvider router={router}/>
 )
