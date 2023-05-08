7daa8f87ff400b34f432681a6a7f59a736dc64e1
YesicaChui <yesicachuic@gmail.com>
2023-05-08 01:22:12 -0500

    maquetando pedidos y lista pedidos

--- src/components/Mozo/ListaPedidos.jsx
+++ src/components/Mozo/ListaPedidos.jsx
@@ -5,7 +5,53 @@ export const ListaPedidos = () => {
   return (
     <>
       <NavMozo />
-      <div>ListaPedidos</div>
+      <main>
+        <section className="productos-orden d-flex justify-content-between flex-wrap">
+          <article className="card mx-auto align-self-start mt-2">
+            <h2 className="card-header bg-warning text-white">Pendiente</h2>
+            <div className="card-body">
+              <h5 className="card-title">Willy</h5>
+              <p className="card-text">2023-05-01 12:09:48</p>
+              <p className="card-text fw-bold">Pedido</p>
+              <ul className="product-pedidos list-group ">              
+                <li className="list-group-item">Hamburguesa 1</li>
+                <li className="list-group-item">Coca Cola 2</li>
+                <li className="list-group-item">Cafe</li>
+              </ul>
+            </div>
+            <button className="btn btn-danger">Cancelar</button>
+          </article>
+          <article className="card mx-auto  align-self-start mt-2">
+            <h2 className="card-header bg-success text-white">Listo</h2>
+            <div className="card-body">
+              <h5 className="card-title">Vilma</h5>
+              <p className="card-text">2023-05-02 10:09:48</p>
+              <p className="card-text fw-bold">Pedido</p>
+              <ul className="product-pedidos list-group ">
+                <li className="list-group-item">Pedido</li>
+                <li className="list-group-item">Chocalte 1</li>
+                <li className="list-group-item">Sandwich de Pollo 1<span>2</span></li>
+              </ul>
+            </div>
+            <button className="btn btn-primary">Entregar</button>
+          </article>
+          <article className="card mx-auto align-self-start mt-2">
+            <h2 className="card-header bg-warning text-white">Pendiente</h2>
+            <div className="card-body">
+              <h5 className="card-title">Yesica</h5>
+              <p className="card-text">2023-05-03 9:09:48</p>
+                <p className="card-text fw-bold">Pedido</p>
+              <ul className="product-pedidos list-group ">
+                <li className="list-group-item">Pedido</li>
+                <li className="list-group-item">Hot Dog 1<span>1</span></li>
+                <li className="list-group-item">Coca Cola 2<span>2</span></li>
+                <li className="list-group-item">Leche 3</li>
+              </ul>
+            </div>
+            <button className="btn btn-danger">Cancelar</button>
+          </article>
+        </section>
+      </main>
     </>
   )
 }
--- src/components/Mozo/Pedidos.jsx
+++ src/components/Mozo/Pedidos.jsx
@@ -1,11 +1,97 @@
 import React from 'react'
 import { NavMozo } from './NavMozo'
+import coffee from '../../assets/coffee.png'
 
 export const Pedidos = () => {
   return (
     <>
       <NavMozo />
-      <div>Pedidos</div>
+      <div className="container">
+        <main className="d-flex justify-content-between">
+          <section className="productos-orden d-flex justify-content-between flex-wrap col-6">
+            <article className="card mx-auto text-center align-self-start mt-2 ">
+              <img className="imgProductoMediano mx-auto mt-2" src={coffee} alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Cafe</h5>
+                <p className="card-text">10.0</p>
+              </div>
+            </article>
+            <article className="card mx-auto text-center align-self-start mt-2">
+              <img className="imgProductoMediano mx-auto mt-2" src={coffee} alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Cafe</h5>
+                <p className="card-text">10.0</p>
+              </div>
+            </article>
+            <article className="card mx-auto text-center align-self-start mt-2 ">
+              <img className="imgProductoMediano mx-auto mt-2" src={coffee} alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Cafe</h5>
+                <p className="card-text">10.0</p>
+              </div>
+            </article>
+            <article className="card mx-auto text-center align-self-start mt-2">
+              <img className="imgProductoMediano mx-auto mt-2" src={coffee} alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Cafe</h5>
+                <p className="card-text">10.0</p>
+              </div>
+            </article>
+
+
+
+          </section>
+          <section className="col-6 mt-2" >
+            <div className="row ">
+              <div className="w-100 px-2">
+                <label htmlFor="cliente" className="fw-bold">Cliente</label>
+                <input id="cliente" type="text" className="form-control mb-2" />
+                <table className="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>Imagen</th>
+                      <th>Precio</th>
+                      <th>Cantidad</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <img src={coffee} alt="Imagen del producto" className="imgProducto" />
+                      </td>
+                      <td className="align-middle">5.0</td>
+                      <td className="align-middle">3</td>
+                      <td className="align-middle">
+                        <button className="btn btn-warning btn-sm ms-1"><i className="bi bi-dash" /></button>
+                        <button className="btn btn-danger btn-sm ms-1"><i className="bi bi-plus" /></button>
+                        <button className="btn btn-danger btn-sm ms-1"><i className="bi bi-trash3" /></button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <img src={coffee} alt="Imagen del producto" className="imgProducto" />
+                      </td>
+                      <td className="align-middle">5.0</td>
+                      <td className="align-middle">3</td>
+                      <td className="align-middle">
+                        <button className="btn btn-warning btn-sm ms-1"><i className="bi bi-dash" /></button>
+                        <button className="btn btn-danger btn-sm ms-1"><i className="bi bi-plus" /></button>
+                        <button className="btn btn-danger btn-sm ms-1"><i className="bi bi-trash3" /></button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <p className="text-end fw-bold">
+                  Total: S/.<span>10.0</span>
+                </p>
+                <button className="btn btn-primary btn-lg me-2">Enviar a Cocina</button>
+
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+
     </>
   )
 }
